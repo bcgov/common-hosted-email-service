31702df7c64871e08d5f0630334e96d27742c145
Jeremy Ho <jujaga@gmail.com>
2019-08-21 15:02:44 -0700

    Refactor email endpoint into component Change success from 200 to 201
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/src/components/email.js
+++ app/src/components/email.js
@@ -1,3 +1,53 @@
-const email = {};
+const log = require('npmlog');
+const nodemailer = require('nodemailer');
+
+const utils = require('./utils');
+
+const email = {
+  /** Transforms a message object into a nodemailer envelope */
+  createEnvelope: message => {
+    const envelope = utils.filterUndefinedAndEmpty(message);
+    // Reassign the body field into the type specified by bodyType
+    delete Object.assign(envelope, {[message.bodyType]: envelope['body'] })['body'];
+    // Remove the bodyType property
+    delete envelope['bodyType'];
+    return envelope;
+  },
+
+  /** Creates an email and sends it to the Ethereal fake SMTP server for viewing */
+  sendMailEthereal: async message => {
+    try {
+      // Generate test SMTP service account from ethereal.email
+      // Only needed if you don't have a real mail account for testing
+      const testAccount = await nodemailer.createTestAccount();
+      log.debug(utils.prettyStringify(testAccount));
+
+      // Create reusable transporter object using the default SMTP transport
+      const transporter = nodemailer.createTransport({
+        host: testAccount.smtp.host,
+        port: testAccount.smtp.port,
+        auth: {
+          user: testAccount.user,
+          pass: testAccount.pass
+        }
+      });
+
+      const envelope = email.createEnvelope(message);
+
+      // Send mail with defined transport object
+      const info = await transporter.sendMail(envelope);
+
+      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+      log.info('Message sent', info.messageId);
+      // Preview only available when sending through an Ethereal account
+      log.info('Preview URL', nodemailer.getTestMessageUrl(info));
+
+      return nodemailer.getTestMessageUrl(info);
+    } catch (error) {
+      log.error('sendMailEthereal', error.message);
+      throw error;
+    }
+  }
+};
 
 module.exports = email;
--- app/src/routes/v1/email.js
+++ app/src/routes/v1/email.js
@@ -1,15 +1,11 @@
-const log = require('npmlog');
 const emailRouter = require('express').Router();
-const nodemailer = require('nodemailer');
 const Problem = require('api-problem');
 const {
   body,
   validationResult
 } = require('express-validator');
 
-// eslint-disable-next-line no-unused-vars
 const emailComponent = require('../../components/email');
-const utils = require('../../components/utils');
 
 // pushes a message
 emailRouter.post('/', [
@@ -29,47 +25,13 @@ emailRouter.post('/', [
   }
 
   try {
-    // const recipients = req.body.recipients.join(', ');
-    // const sender = '"Common Service Showcase 🦜" <NR.CommonServiceShowcase@gov.bc.ca>';
-
-    if (req.body.devMode) {
-      // Generate test SMTP service account from ethereal.email
-      // Only needed if you don't have a real mail account for testing
-      const testAccount = await nodemailer.createTestAccount();
-      log.debug(utils.prettyStringify(testAccount));
-
-      // create reusable transporter object using the default SMTP transport
-      const transporter = nodemailer.createTransport({
-        host: testAccount.smtp.host,
-        port: testAccount.smtp.port,
-        auth: {
-          user: testAccount.user,
-          pass: testAccount.pass
-        }
-      });
-
-      const envelope = {
-        from: req.body.from,
-        to: req.body.to,
-        subject: req.body.subject,
-        text: req.body.body
-      };
-
-      // send mail with defined transport object
-      const info = await transporter.sendMail(envelope);
-
-      log.info('Message sent', info.messageId);
-      // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-      // Preview only available when sending through an Ethereal account
-      log.info('Preview URL', nodemailer.getTestMessageUrl(info));
-      // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-      res.status(200).send(nodemailer.getTestMessageUrl(info));
+    if (req.query.devMode) {
+      const result = await emailComponent.sendMailEthereal(req.body);
+      res.status(201).json(result);
     } else {
       res.status(200).end();
     }
   } catch (error) {
-    log.error(error.message);
     new Problem(502, {
       detail: error.message
     }).send(res);
