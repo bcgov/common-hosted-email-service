62e0da7efd2f3d59a2ab02643fc7f28f97e01efc
Jeremy Ho <jujaga@gmail.com>
2019-09-24 11:04:14 -0700

    Attempt to fix strange connection issue on tests
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/tests/unit/routes/v1.spec.js
+++ app/tests/unit/routes/v1.spec.js
@@ -3,6 +3,8 @@ const request = require('supertest');
 
 const router = require('../../../src/routes/v1');
 
+jest.mock('bull');
+
 // Simple Express Server
 const basePath = '/api/v1';
 const app = express();
@@ -14,7 +16,7 @@ app.use(basePath, router);
 
 describe('GET /api/v1', () => {
   it('should return all available endpoints', async () => {
-    const response = await request(app).get('/api/v1');
+    const response = await request(app).get(`${basePath}`);
 
     expect(response.statusCode).toBe(200);
     expect(response.body).toBeTruthy();
@@ -29,7 +31,7 @@ describe('GET /api/v1', () => {
 
 describe('GET /api/v1/docs', () => {
   it('should return a redoc html page', async () => {
-    const response = await request(app).get('/api/v1/docs');
+    const response = await request(app).get(`${basePath}/docs`);
 
     expect(response.statusCode).toBe(200);
     expect(response.text).toMatch(/<title>Common Hosted Email Service API - Documentation/);
@@ -38,7 +40,7 @@ describe('GET /api/v1/docs', () => {
 
 describe('GET /api/v1/api-spec.yaml', () => {
   it('should return the OpenAPI yaml spec', async () => {
-    const response = await request(app).get('/api/v1/api-spec.yaml');
+    const response = await request(app).get(`${basePath}/api-spec.yaml`);
 
     expect(response.statusCode).toBe(200);
     expect(response.text).toMatch(/openapi: 3.0.0/);
