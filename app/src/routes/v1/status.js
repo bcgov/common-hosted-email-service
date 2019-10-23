4f0efb25818987962fd0e9e2751e078c830822fd
Jeremy Ho <jujaga@gmail.com>
2019-10-23 11:34:23 -0700

    Add stub /status endpoint with validation support
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/src/routes/v1/status.js
+++ app/src/routes/v1/status.js
@@ -1,11 +1,25 @@
 const ChesService = require('../../services/chesSvc');
+const { validateStatusQuery } = require('../../middleware/validation');
 
 const statusRouter = require('express').Router();
+const chesService = new ChesService();
+
+statusRouter.get('/', validateStatusQuery, async (req, res, next) => {
+  try {
+    const { fields, ...params } = req.query;
+
+    // Return
+    res.status(200).json({
+      fields: fields,
+      params: params
+    });
+  } catch (err) {
+    next(err);
+  }
+});
 
 statusRouter.get('/:msgId', async (req, res, next) => {
   try {
-    const chesService = new ChesService();
-
     // transform message and statuses into API format...
     const status = await chesService.getStatus(req.authorizedParty, req.params.msgId);
 
