fa24cb61ccc57c23c816465700a9616cfd3b6c89
Jeremy Ho <jujaga@gmail.com>
2020-08-10 12:25:40 -0700

    Add nodemailer-openpgp plugin support to main sendMail service function
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/src/services/emailSvc.js
+++ app/src/services/emailSvc.js
@@ -11,6 +11,7 @@
  * @exports EmailService
  */
 const log = require('npmlog');
+const openpgpEncrypt = require('nodemailer-openpgp').openpgpEncrypt;
 
 const utils = require('../components/utils');
 
@@ -21,14 +22,14 @@ class EmailService {
    * Creates a new EmailService with default connection.
    * @class
    */
-  constructor () {
+  constructor() {
     this.connection = new EmailConnection();
   }
 
   /** @function connection
    *  Gets the current EmailConnection
    */
-  get connection () {
+  get connection() {
     return this._connection;
   }
 
@@ -36,7 +37,7 @@ class EmailService {
    *  Sets the current EmailConnection
    *  @param {object} v - an EmailConnection
    */
-  set connection (v) {
+  set connection(v) {
     this._connection = v;
   }
 
@@ -44,7 +45,7 @@ class EmailService {
    *  @param {object} message - a Message (email)
    *  @returns {object} - the nodemailer message
    */
-  createEnvelope (message) {
+  createEnvelope(message) {
     const envelope = utils.filterUndefinedAndEmptyArray(message);
     // Reassign the body field into the type specified by bodyType
     delete Object.assign(envelope, {
@@ -60,9 +61,17 @@ class EmailService {
    *  @param {object} mailer - a nodemailer transport
    *  @param {object} message - the nodemailer message
    */
-  async sendMail (mailer, message) {
+  async sendMail(mailer, message) {
     try {
       const envelope = this.createEnvelope(message);
+
+      // Add PGP Encryption stream plugin if necessary
+      if (envelope.encryptionKeys) {
+        mailer.use('stream', openpgpEncrypt({
+          signingKey: envelope.signingKey,
+          passphrase: envelope.signingKeyPassphrase
+        }));
+      }
 
       // Send mail with defined transport object
       const info = await mailer.sendMail(envelope);
@@ -81,7 +90,7 @@ class EmailService {
    *  @param {boolean} ethereal - when true, send to Ethereal service (good for local testing)
    *  @returns {object} A nodemailer result object
    */
-  async send (message, ethereal = false) {
+  async send(message, ethereal = false) {
     if (ethereal) {
       const etherealConnection = await EmailConnection.getEtherealConnection();
       const info = await this.sendMail(etherealConnection.mailer, message);
