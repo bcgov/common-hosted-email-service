d2056ef99e6ea526563c365f518f660581ca06e0
Jeremy Ho <jujaga@gmail.com>
2020-08-12 09:55:19 -0700

    Dynamically generate openIdConnectUrl contents based on configuration Add JSON representation for OpenAPI spec
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/src/routes/v1.js
+++ app/src/routes/v1.js
@@ -1,6 +1,8 @@
 const config = require('config');
+const fs = require('fs');
 const router = require('express').Router();
 const path = require('path');
+const yaml = require('js-yaml');
 
 const keycloak = require('../components/keycloak');
 const { authorizedPartyValidator } = require('../middleware/authorizedParty');
@@ -12,6 +14,13 @@ const mergeRouter = require('./v1/merge');
 const statusRouter = require('./v1/status');
 
 const clientId = config.get('keycloak.clientId');
+
+const getSpec = () => {
+  const rawSpec = fs.readFileSync(path.join(__dirname, '../docs/v1.api-spec.yaml'), 'utf8');
+  const spec = yaml.safeLoad(rawSpec);
+  spec.components.securitySchemes.OpenID.openIdConnectUrl = `${config.get('keycloak.serverUrl')}/realms/${config.get('keycloak.realm')}/.well-known/openid-configuration`;
+  return spec;
+};
 
 /** Base v1 Responder */
 router.get('/', (_req, res) => {
@@ -36,7 +45,12 @@ router.get('/docs', (_req, res) => {
 
 /** OpenAPI YAML Spec */
 router.get('/api-spec.yaml', (_req, res) => {
-  res.sendFile(path.join(__dirname, '../docs/v1.api-spec.yaml'));
+  res.status(200).type('application/yaml').send(yaml.safeDump(getSpec()));
+});
+
+/** OpenAPI JSON Spec */
+router.get('/api-spec.json', (_req, res) => {
+  res.status(200).json(getSpec());
 });
 
 /** Health Router */
