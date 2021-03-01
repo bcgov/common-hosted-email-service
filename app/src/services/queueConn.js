41bf84069f5869b6128bd27a22ba2e69c1e757bb
Jeremy Ho <jujaga@gmail.com>
2021-03-01 15:01:53 -0800

    Apply minimal PoC changes for Redis Cluster testing
    Signed-off-by: Jeremy Ho <jujaga@gmail.com>

--- app/src/services/queueConn.js
+++ app/src/services/queueConn.js
@@ -12,9 +12,8 @@
 const Bull = require('bull');
 const config = require('config');
 const log = require('npmlog');
+const Redis = require('ioredis');
 const utils = require('../components/utils');
-
-let globalQueue;
 
 class QueueConnection {
   /**
@@ -22,16 +21,46 @@ class QueueConnection {
    * @class
    */
   constructor() {
-    const configuration = {
-      // Prefix must be explicitly defined with brackets to support Redis Clustering
-      // https://github.com/OptimalBits/bull/blob/master/PATTERNS.md#redis-cluster
-      prefix: '{bull}',
-      redis: {
-        host: config.get('redis.host'),
-        password: config.get('redis.password')
-      }
-    };
-    this.queue = new Bull('ches', configuration);
+    if (!QueueConnection.instance) {
+      this._connected = false;
+      this.queue = new Bull('ches', {
+        // Prefix must be explicitly defined with brackets to support Redis Clustering
+        // https://github.com/OptimalBits/bull/blob/master/PATTERNS.md#redis-cluster
+        prefix: '{bull}',
+        createClient: () => {
+          // The Cluster instance must be created inside the function to behave correctly
+          // https://github.com/OptimalBits/bull/issues/1401#issuecomment-519443898
+          const cluster = new Redis.Cluster([{
+            host: config.get('redis.host'),
+            port: 6379
+          }], {
+            redisOptions: {
+              password: config.get('redis.password')
+            }
+          });
+
+          cluster.on('error', (error) => {
+            log.error(`Redis Errored: ${error}`);
+          });
+          cluster.on('end', (error) => {
+            log.error(`Redis Ended: ${error}`);
+          });
+          cluster.on('ready', () => {
+            log.debug('Redis Ready.');
+          });
+          cluster.on('connect', () => {
+            log.debug('Redis Connected');
+            this._connected = true;
+          });
+
+          return cluster;
+        }
+      });
+
+      QueueConnection.instance = this;
+    }
+
+    return QueueConnection.instance;
   }
 
   /**
@@ -50,8 +79,7 @@ class QueueConnection {
    */
   set queue(v) {
     this._queue = v;
-    this._connected = false;
-    globalQueue = this._queue;
+    // this._connected = false;
   }
 
   /** @function connected
@@ -63,12 +91,12 @@ class QueueConnection {
 
   /**
    *  @function close
-   *  Will close the globally stored QueueConnection
+   *  Will close the QueueConnection
    */
   static close() {
-    if (globalQueue) {
+    if (this.queue) {
       try {
-        globalQueue.close();
+        this.queue.close();
         this._connected = false;
         log.info('QueueConnection', 'Disconnected');
       } catch (e) {
@@ -80,20 +108,21 @@ class QueueConnection {
   /**
    *  @function checkConnection
    *  Checks the current QueueConnection
-   *  @param {integer} [timeout=1] Number of seconds to retry before failing out
+   *  @param {integer} [timeout=5] Number of seconds to retry before failing out
    *  @returns boolean True if queue is connected
    */
   async checkConnection(timeout = 5) {
     // Redis does not establish connection immediately.
     // You need a small grace period checking for the status.
     for (let i = 0; i < timeout; i++) {
-      this._connected = this.queue.clients[0].status === 'ready';
+      // this._connected = this.queue.clients[0].status === 'ready';
       if (this.connected) break;
       await utils.wait(1000);
     }
     if (!this.connected) {
       log.error('QueueConnection', 'Unable to connect to queue');
     }
+
     return this._connected;
   }
 }
