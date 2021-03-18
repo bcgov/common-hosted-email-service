a76bfe996154c2441692f5adaebae2cb0c9cb5a5
Lucas O'Neil <lucasoneil@gmail.com>
2021-03-18 15:11:48 -0700

    Paremeters

--- load-tests/script.js
+++ load-tests/script.js
@@ -6,12 +6,13 @@ import { check, fail } from 'k6';
 // 1. Init
 // -------------------------------------------------------------------------------------------------
 const client = 'TEST_SERVICE_CLIENT';
-const senderEmail = 'donotreplyCHES@extest.gov.bc.ca';
+const senderEmail = `${__ENV.sender ? __ENV.sender : 'donotreplyCHES@extest.gov.bc.ca'}`;
 // https://k6.io/docs/using-k6/environment-variables
 const secret = `${__ENV.secret}`;
 const env = `${__ENV.env ? __ENV.env : 'dev'}`;
 const targetEmail = `${__ENV.target}`
-const emailUrl = 'https://ches-dev.apps.silver.devops.gov.bc.ca/api/v1/email';
+let emailUrl = 'api/v1/email';
+let oidcUrl = 'auth/realms/jbd6rnxw/protocol/openid-connect/token';
 
 // Options for the test run. These can be overidden for a run. See https://k6.io/docs/using-k6/options
 export let options = {
@@ -25,15 +26,31 @@ export let options = {
   },
   userAgent: 'CommonServicesTeamLoadTestK6/1.0',
 
-  // K6 vars, can overload in a command
+  // K6 vars, overload in the command
   iterations: 1,
   vus: 1
 };
+
+// URL strings
+if (env === 'dev' || env === 'test') {
+  emailUrl = `https://ches-${env}.apps.silver.devops.gov.bc.ca/${emailUrl}`;
+  oidcUrl = `https://${env}.oidc.gov.bc.ca/${oidcUrl}`
+} else if (env === 'prod') {
+  emailUrl = `https://ches.nrs.gov.bc.ca/${emailUrl}`;
+  oidcUrl = `https://oidc.gov.bc.ca/${oidcUrl}`
+} else if (env && env.match(/pr-\d+/g)) {
+  emailUrl = `https://ches-dev-${env}.nrs.gov.bc.ca/${emailUrl}`;
+  oidcUrl = `https://dev.oidc.gov.bc.ca/${oidcUrl}`
+} else {
+  fail('No valid environment supplied. Supply "dev", "test", "prod", or "pr##".');
+}
 
 // -------------------------------------------------------------------------------------------------
 // 2. Setup
 // -------------------------------------------------------------------------------------------------
 export function setup() {
+  console.log(`Email url: ${emailUrl}`);
+  console.log(`SSO url: ${oidcUrl}`);
   console.debug('Getting Token');
   // Check a secret was supplied or nothing will work
   var pattern = new RegExp('^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$', 'i');
@@ -42,7 +59,6 @@ export function setup() {
   }
 
   // Get a token first to use in the run (only lasts 5 minutes)
-  const oidcUrl = 'https://dev.oidc.gov.bc.ca/auth/realms/jbd6rnxw/protocol/openid-connect/token';
   const oidcPayload = {
     grant_type: 'client_credentials',
   };
@@ -103,17 +119,14 @@ export default function (data) {
     ]
   };
 
-  const emailRes = http.post('https://ches-dev.apps.silver.devops.gov.bc.ca/api/v1/email', JSON.stringify(payload), options);
+  const emailRes = http.post(emailUrl, JSON.stringify(payload), options);
   console.debug(JSON.stringify(emailRes, 0, 2));
-  if(!check(emailRes, {
+  if (!check(emailRes, {
     'email response is 201': (r) => r.status === 201,
     'email response has expected body': (r) => r.json("messages")
   })) {
     console.warn(JSON.stringify(emailRes, 0, 2));
   }
-
-  // const msgId = emailRes.json("messages.0.msgId");
-  // console.log(msgId);
 }
 
 
